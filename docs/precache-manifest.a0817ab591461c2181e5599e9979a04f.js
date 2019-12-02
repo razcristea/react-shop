877959578d6ab18596dc07adcd09cbc0ec9da17e
Razvan Cristea <razcristea@gmail.com>
2019-12-02 18:54:38 +0200

    build deploy

---
+++ docs/precache-manifest.a0817ab591461c2181e5599e9979a04f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c4c021a58f1f5976a3cc2dfb63670b",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "4d713d3492295761f330",
+    "url": "/react-shop/static/css/main.4a9ad509.chunk.css"
+  },
+  {
+    "revision": "e0c9e7d616309c9e28d1",
+    "url": "/react-shop/static/js/2.791743f7.chunk.js"
+  },
+  {
+    "revision": "4d713d3492295761f330",
+    "url": "/react-shop/static/js/main.2577abf7.chunk.js"
+  },
+  {
+    "revision": "ee7da6477f74380c0170",
+    "url": "/react-shop/static/js/runtime-main.379c7871.js"
+  }
+]);
